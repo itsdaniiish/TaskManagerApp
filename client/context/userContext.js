f7533c51a15debc585f30150b02b51938690143e
Danish <speak2danishofficially.com>
2024-09-23 16:05:38 +0530

    updated

--- client/context/userContext.js
+++ client/context/userContext.js
@@ -9,7 +9,7 @@ const UserContext = React.createContext();
 axios.defaults.withCredentials = true;
 
 export const UserContextProvider = ({ children }) => {
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://tmbackend-4.onrender.com";
   const router = useRouter();
   const [user, setUser] = useState({});
   const [allUsers, setAllUsers] = useState([]);
